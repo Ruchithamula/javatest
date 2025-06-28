70070b2d5912bdfe38ad1195a33e46fe27903819
Ruchithamula <mularuchitha14@gmail.com>
2025-06-28 12:16:40 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,10 @@
+class add{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);{
+      int a=5;
+      int b=6;
+      int c=a+b;
+      System.out.println(c);
+    }
+  }
+}
